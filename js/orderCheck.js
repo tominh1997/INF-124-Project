e9f0ad078cc482faeb5203b84d393970f306840f
Kuljit Singh <34052579+kuljits1@users.noreply.github.com>
2020-04-20 15:46:24 -0700

    Updated Alerts with New line and empty strings

--- js/orderCheck.js
+++ js/orderCheck.js
@@ -38,47 +38,85 @@ function checkValidInputs(){
     if(!isNaN(productVal)){
         errorMsgs.push("Please Enter a String not an Integer for Product Value"); 
     }
+
+    if(productVal == '' || productVal == null){ 
+        errorMsgs.push("Please Fill in Order Product");
+    }
     
     if(!isNaN(firstNameVal)){
         errorMsgs.push("Please Enter a String not an Integer for First Name"); 
+    }
+    
+    if(firstNameVal == '' || firstNameVal == null){ 
+        errorMsgs.push("Please Fill in First Name");
     }
     
     if(!isNaN(lastNameVal)){
         errorMsgs.push("Please Enter a String not an Integer for Last Name"); 
     }
     
+    if(lastNameVal == '' || lastNameVal == null){ 
+        errorMsgs.push("Please Fill in Last Name");
+    }
+    
     if (phoneVal.length > 10 || phoneVal.length < 10 || isNaN(phoneVal) ) { 
         errorMsgs.push("Please Enter a Valid Phone Number");  
+    }
+    
+    if(phoneVal == '' || phoneVal == null){ 
+        errorMsgs.push("Please Fill in Phone Number");
     }
     
     if(!isNaN(cityVal)){
         errorMsgs.push("Please Enter a String not an Integer for City"); 
     }
     
+    if(cityVal == '' || cityVal == null){ 
+        errorMsgs.push("Please Fill in City");
+    }
+    
     if(!isNaN(stateVal) ){
         errorMsgs.push("Please Enter a String not an Integer for State"); 
+    }
+    
+    if(stateVal == '' || stateVal == null){ 
+        errorMsgs.push("Please Fill in State");
     }
     
     if(!isNaN(countryVal)){
         errorMsgs.push("Please Enter a String not an Integer for Country"); 
     }
     
+    if(countryVal == '' || countryVal == null){ 
+        errorMsgs.push("Please Fill in Country");
+    }
+    
     if (zipVal.length > 5 || zipVal.length < 5 || isNaN(zipVal)) {
         errorMsgs.push("Please Enter a Valid Zip Code"); 
-
+    }
+    
+    if(zipVal == '' || zipVal == null){ 
+        errorMsgs.push("Please Fill in Zip Code");
     }
     
     if (cardNumVal.length > 16 || cardNumVal.length < 16 || isNaN(cardNumVal)) { 
         errorMsgs.push("Please Enter a Valid Credit Card Number"); 
-
+    }
+    
+    if(cardNumVal == '' || cardNumVal == null){ 
+        errorMsgs.push("Please Fill in Card Number");
     }
     
     if (cvvVal.length > 3 || cvvVal.length < 3 || isNaN(cvvVal)) { 
         errorMsgs.push("Please Enter a Valid CVV");  
     }
     
+    if(cvvVal == '' || cvvVal == null){ 
+        errorMsgs.push("Please Fill in CVV");
+    }
+    
     if(errorMsgs.length > 0){ 
-        alert(errorMsgs.join(", ")); 
+        alert(errorMsgs.join("\r\n")); 
         return true; 
     }
 
