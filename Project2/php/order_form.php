9fb95c7448c571700fca42406248c1ebe3530154
HuyDucVo <hdvo1@uci.edu>
2020-05-03 00:17:58 -0700

    Update order_form.php

--- Project2/php/order_form.php
+++ Project2/php/order_form.php
@@ -1,5 +1,4 @@
 <?php
-ob_start();
 $pdo=new PDO('mysql:host=localhost:3306;dbname=chocoholic_db','root', '');
 $sql ="INSERT INTO orders VALUES(NULL, :buyer_name, :phone_number,
                  :product_id, :product_name, :quantity, :total_amount,
@@ -14,11 +13,11 @@ $stmt->execute(array(':buyer_name' => $_POST["firstName"],
                      ':quantity' => $_POST["quantity"],
                      ':total_amount' => $total_amount,
                      ':address' => $_POST["adrress"],
-                     ':shipping_method' => $_POST["shipping method"],
+                     ':shipping_method' => $_POST["shipping_method"],
                      ':credit_card_number' => $_POST["cardnum"],
                      ':CVV' => $_POST["cvv"],
-                    ));
-ob_end_flush();                    
-header("Location: http://".$_SERVER['HTTP_HOST']."/confirmation.html?buyer_name=".$_POST["firstName"]."&total_amount=".$total_amount);
-exit(0); 
+                    ));          
+print_r($_POST);         
+//header("Location: http://".$_SERVER['HTTP_HOST']."/confirmation.html?buyer_name=".$_POST["firstName"]."&total_amount=".$total_amount);
+//exit(0); 
 ?>
