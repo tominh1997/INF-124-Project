07b14342b0f35e7c0379b81e7a57515e292aaaf0
tominh1997 <mcto@uci.edu>
2020-05-03 18:30:38 -0700

    Update productDescription.php

--- Project2/productDescription.php
+++ Project2/productDescription.php
@@ -53,7 +53,7 @@
   <div class="subnav-right">
     <button class="subnavbtn">Products <i class="fa fa-caret-down"></i></button>
     <div class="subnav-content">
-        <a href="products.php?type=white"> White Chocolate</a>
+    <a href="products.php?type=white"> White Chocolate</a>
       <a href="products.php?type=dark">Dark Chocolate</a>
       <a href="products.php?type=milk">Milk Chocolate</a>
     </div>
