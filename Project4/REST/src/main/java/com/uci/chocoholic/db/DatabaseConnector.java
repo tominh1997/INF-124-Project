6df0870f2a5eec86866feea24abe82378fd5d73c
tominh1997 <mcto@uci.edu>
2020-05-31 00:45:44 -0700

    Update DatabaseConnector.java

--- Project4/REST/src/main/java/com/uci/chocoholic/db/DatabaseConnector.java
+++ Project4/REST/src/main/java/com/uci/chocoholic/db/DatabaseConnector.java
@@ -9,24 +9,6 @@ public class DatabaseConnector {
     private DatabaseConnector() {
 
     }
-    /*
-    public static Connection getConnection() {
-
-        try {
-            Class.forName("org.mariadb.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            return DriverManager.getConnection("jdbc:mariadb://192.168.0.209/db4lecture",
-                    "root", "mytest");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-        return null;
-    }
-    */
     public static Connection initializeDatabase()
             throws SQLException, ClassNotFoundException
     {
