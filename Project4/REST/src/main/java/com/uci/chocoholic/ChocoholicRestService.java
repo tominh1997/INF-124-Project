09064b842ed9518e41afdbcbdee6c003f891a5c8
tominh1997 <mcto@uci.edu>
2020-05-31 15:19:51 -0700

    Temporary changes for testing REST

--- Project4/REST/src/main/java/com/uci/chocoholic/ChocoholicRestService.java
+++ Project4/REST/src/main/java/com/uci/chocoholic/ChocoholicRestService.java
@@ -1,16 +1,48 @@
 package com.uci.chocoholic;
+
+import com.uci.chocoholic.db.DatabaseConnector;
+import com.uci.chocoholic.db.DatabaseUtils;
 import com.uci.chocoholic.model.Item;
 import com.uci.chocoholic.service.CheckOutService;
-import com.uci.chocoholic.service.ChocoholicService;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 @Path("/chocoholic")
-public class ChocoholicResource {
+public class ChocoholicRestService {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("{type}")
+    public String getItemsByType(@PathParam("type") String type) throws SQLException, ClassNotFoundException {
+        Connection dbcon = DatabaseConnector.initializeDatabase();
+        ArrayList<Item> products = DatabaseUtils.retrieveItemsByType(dbcon, type);
+        String productNames = "";
+        for (Item item: products){
+            productNames += item.getName() + "\n";
+        }
+        return productNames;
+//        if (products.size() == 0) {
+//            return Response.status(Response.Status.NOT_FOUND).build();
+//        }
+//        return Response.ok(products).build();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/product/{id}")
+    public String getItemByID(@PathParam("id") String id) throws SQLException, ClassNotFoundException {
+        Connection dbcon = DatabaseConnector.initializeDatabase();
+        Item product = DatabaseUtils.retrieveItemByID(dbcon, id);
+        return product.getName();
+//        if (product == null) {
+//            return Response.status(Response.Status.NOT_FOUND).build();
+//        }
+//        return Response.ok(product).build();
+    }
 
     @POST
     @Consumes({MediaType.APPLICATION_JSON}) //This method accepts a request of the JSON type
@@ -34,7 +66,6 @@ public class ChocoholicResource {
 
         // Return an Internal Server error because something wrong happened. This should never be executed
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-
     }
 
 }
--- Project4/REST/src/main/java/com/uci/chocoholic/db/DatabaseUtils.java
+++ Project4/REST/src/main/java/com/uci/chocoholic/db/DatabaseUtils.java
@@ -1,42 +1,79 @@
 package com.uci.chocoholic.db;
 
-import java.sql.*;
+import com.uci.chocoholic.model.Item;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 
 public class DatabaseUtils {
 
-
-    public static ResultSet retrieveQueryResults(Connection connection, final String sql) {
-        Statement statement = null;
+    private static DecimalFormat df2 = new DecimalFormat("#.##");
+    public static ArrayList<Item> retrieveItemsByType(Connection connection, final String type) {
+        ArrayList<Item> products = new ArrayList<Item>();
         try {
-            statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-            return resultSet;
+            String query = "SELECT * from chocoholic_db.products where type = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            // Set the parameter represented by "?" in the query to the id we get from url,
+            // num 1 indicates the first "?" in the query
+            statement.setString(1, type.toLowerCase());
+            ResultSet rs = statement.executeQuery();
+            // Iterate through each row of rs
+            while (rs.next()) {
+                Item product = new Item();
+                product.setName(rs.getString("name"));
+                product.setId(rs.getString("id"));
+                product.setDescription(rs.getString("description"));
+                product.setType(type);
+                product.setPrice(Double.parseDouble(df2.format(rs.getDouble("price"))));
+                product.setImage1(product.convertToBase64(rs.getBlob("image1")));
+                product.setImage2(product.convertToBase64(rs.getBlob("image2")));
+                product.setImage3(product.convertToBase64(rs.getBlob("image3")));
+                products.add(product);
+            }
+            connection.close();
+            rs.close();
+            statement.close();
+        } catch (SQLException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return products;
+    }
 
-        } catch (SQLException e) {
+    public static Item retrieveItemByID(Connection connection, final String id) {
+        Item product = new Item();
+        try {
+            String query = "SELECT * from chocoholic_db.products where id = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            // Set the parameter represented by "?" in the query to the id we get from url,
+            // num 1 indicates the first "?" in the query
+            statement.setInt(1, Integer.parseInt(id));
+            ResultSet rs = statement.executeQuery();
+            // Iterate through each row of rs
+            rs.next();
+            product.setName(rs.getString("name"));
+            product.setId(rs.getString("id"));
+            product.setDescription(rs.getString("description"));
+            product.setType(rs.getString("type"));
+            product.setPrice(Double.parseDouble(df2.format(rs.getDouble("price"))));
+            product.setImage1(product.convertToBase64(rs.getBlob("image1")));
+            product.setImage2(product.convertToBase64(rs.getBlob("image2")));
+            product.setImage3(product.convertToBase64(rs.getBlob("image3")));
+            connection.close();
+            rs.close();
+            statement.close();
+            return product;
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return null;
-
     }
 
-    public static ResultSet retrieveQueryResultsWithParam(Connection connection, final String sql, Object param) {
-
-        PreparedStatement statement = null;
-        try {
-            statement = connection.prepareStatement(sql);
-            statement.setInt(1, (Integer) param);
-            ResultSet resultSet = statement.executeQuery(sql);
-            return resultSet;
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-        return null;
-
-
-    }
 
     public static boolean performDBUpdate(Connection connection, String sql, String... params) {
         PreparedStatement preparedStatement = null;
